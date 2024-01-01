4ae481fe6a99fa7ce23878bee5fbf9633bc1a6d2
Daniel Dimitrov <ddimitrov1108@gmail.com>
2024-01-01 18:56:18 +0200

    .

--- app/api/label/route.ts
+++ app/api/label/route.ts
@@ -1,47 +1,52 @@
-import { isLabelDuplucated } from "@/db/LabelDb";
-import { authConfig } from "@/lib/auth";
-import { IUserSession } from "@/lib/interfaces";
-import prisma from "@/lib/prisma";
-import { validateLabelValues } from "@/lib/utils";
-import { getServerSession } from "next-auth";
+// import { authConfig } from "@/lib/auth";
+// import { IUserSession } from "@/lib/interfaces";
+// import prisma from "@/lib/prisma";
+// import { validateLabelValues } from "@/lib/utils";
+// import { getServerSession } from "next-auth";
+// import { NextRequest, NextResponse } from "next/server";
+
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const session: IUserSession | null = await getServerSession(authConfig);
-
-  if (!session || !session.user || !session.user.id)
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-
-  const { name } = await req.json();
-
-  if (!validateLabelValues(name))
-    return NextResponse.json({ error: "Invalid fields." }, { status: 400 });
-
-  const formattedName = name.toLowerCase().replace(/\s+/g, "-");
-
-  try {
-    const isDuplicate = await isLabelDuplucated(session.user.id, formattedName);
-
-    if (isDuplicate) {
-      return NextResponse.json(
-        { error: "The label already exists! Please try another name." },
-        { status: 409 }
-      );
-    }
-
-    await prisma.label.create({
-      data: {
-        uid: session.user.id,
-        name: formattedName,
-      },
-    });
-
-    return NextResponse.json({}, { status: 200 });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { error: "Something went wrong. Please try again later." },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json({}, { status: 200 });
 }
+
+// export async function POST(req: NextRequest) {
+//   const session: IUserSession | null = await getServerSession(authConfig);
+
+//   if (!session || !session.user || !session.user.id)
+//     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+//   const { name } = await req.json();
+
+//   if (!validateLabelValues(name))
+//     return NextResponse.json({ error: "Invalid fields." }, { status: 400 });
+
+//   const formattedName = name.toLowerCase().replace(/\s+/g, "-");
+
+//   try {
+//     const isDuplicate = await isLabelDuplucated(session.user.id, formattedName);
+
+//     if (isDuplicate) {
+//       return NextResponse.json(
+//         { error: "The label already exists! Please try another name." },
+//         { status: 409 }
+//       );
+//     }
+
+//     await prisma.label.create({
+//       data: {
+//         uid: session.user.id,
+//         name: formattedName,
+//       },
+//     });
+
+//     return NextResponse.json({}, { status: 200 });
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json(
+//       { error: "Something went wrong. Please try again later." },
+//       { status: 500 }
+//     );
+//   }
+// }
