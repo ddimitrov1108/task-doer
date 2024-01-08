59d0d0161f13393156cddacd060a8fdc09e3b475
Daniel Dimitrov <ddimitrov1108@gmail.com>
2024-01-08 22:32:09 +0200

    fix: provider exports

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,2 +1,5 @@
 export { default as AuthProvider } from "./AuthProvider";
 export { default as TaskProvider } from "./TaskProvider";
+export { default as ProjectProvider } from "./ProjectProvider";
+export { ProjectContext } from "./ProjectProvider";
+export { TaskContext } from "./TaskProvider";
