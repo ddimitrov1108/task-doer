89dedfd8a3a7215e89d2417fd195c61b7593c25e
Daniel Dimitrov <ddimitrov1108@gmail.com>
2024-01-08 22:31:52 +0200

    add: DeleteConfirmationModal

---
+++ components/modals/DeleteConfirmationModal.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { Dispatch, FormEvent, SetStateAction } from "react";
+import { Button, Modal } from "../ui";
+import { useForm } from "../hooks";
+import { AlertTriangle } from "lucide-react";
+
+interface Props {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  message: string;
+  onSubmit: () => Promise<void> | void;
+}
+
+const DeleteConfirmationModal = ({
+  isOpen,
+  setIsOpen,
+  message,
+  onSubmit,
+}: Props) => {
+  const [form, setForm] = useForm();
+
+  const onSubmitHandler = async (e: FormEvent) => {
+    e.preventDefault();
+    setForm({ ...form, loading: true });
+    await onSubmit();
+    setForm({ ...form, loading: false });
+  };
+
+  return (
+    <Modal
+      modalTitle={
+        <div className="flex items-center gap-2">
+          <div className="text-2xl text-main">
+            <AlertTriangle />
+          </div>
+          Delete Confirmation
+        </div>
+      }
+      isOpen={isOpen}
+      setIsOpen={setIsOpen}
+      className="max-w-sm"
+      bodyClassName="pt-4"
+    >
+      <form onSubmit={onSubmitHandler}>
+        <div className="mb-6">{message}</div>
+
+        <div className="flex flex-col-reverse md:flex-row gap-2 justify-between items-center">
+          <Button
+            variant="text"
+            className="flex justify-center"
+            disabled={form.loading}
+            onClick={() => setIsOpen(false)}
+            fullWidth
+          >
+            Cancel
+          </Button>
+
+          <Button
+            variant="error"
+            type="submit"
+            className="flex justify-center"
+            disabled={form.loading}
+            loading={form.loading}
+            fullWidth
+          >
+            Delete
+          </Button>
+        </div>
+      </form>
+    </Modal>
+  );
+};
+export default DeleteConfirmationModal;
--- components/modals/index.tsx
+++ components/modals/index.tsx
@@ -1,1 +1,3 @@
 export { default as ProjectModal } from "./ProjectModal";
+export { default as LabelModal } from "./LabelModal";
+export { default as DeleteConfirmationModal } from "./DeleteConfirmationModal";
