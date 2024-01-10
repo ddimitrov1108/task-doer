04b537efab23c3f932a8255c9a9dfeb296e0c32b
Daniel Dimitrov <ddimitrov1108@gmail.com>
2024-01-10 19:52:58 +0200

    add: deleteProject server action

---
+++ app/actions/deleteProject.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { projectController } from "@/db";
+import { getUserFromServerSession } from "@/lib/auth";
+import { isUUID } from "@/lib/utils";
+import { revalidatePath } from "next/cache";
+
+export default async function deleteProject(project_id: string) {
+  const user = await getUserFromServerSession();
+
+  if (!user) return { error: "Unauthenticated" };
+  if (!isUUID(project_id)) return { error: "Bad Request" };
+
+  try {
+    const deletedProject = await projectController.delete(user.id, project_id);
+
+    if (!deletedProject) throw new Error("Failed to delete project");
+
+    revalidatePath("/todo/project/[id]");
+
+    return {};
+  } catch (e) {
+    console.error(e);
+    return { error: "Something went wrong. Please try again later" };
+  }
+}
--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,2 +1,3 @@
 export { default as createProject } from "./createProject";
 export { default as updateProject } from "./updateProject";
+export { default as deleteProject } from "./deleteProject";
