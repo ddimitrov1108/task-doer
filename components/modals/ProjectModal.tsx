d34a4aa6903be73710aad20bec421771c3ebb862
Daniel Dimitrov <ddimitrov1108@gmail.com>
2024-01-10 22:09:32 +0200

    modified:   components/modals/ProjectModal.tsx

--- components/modals/ProjectModal.tsx
+++ components/modals/ProjectModal.tsx
@@ -1,6 +1,6 @@
 import { FolderEdit, FolderPlus } from "lucide-react";
 import { Modal } from "../ui";
-import { ProjectForm, TaskForm } from "../forms";
+import { ProjectForm } from "../forms";
 import { IFormModal, IProjectFormValues } from "@/lib/interfaces";
 
 const ProjectModal = ({
@@ -25,13 +25,11 @@ const ProjectModal = ({
       className="max-w-sm"
       bodyClassName="pt-4"
     >
-      {/* <ProjectForm
+      <ProjectForm
         editMode={editMode}
         initialState={initialState}
         afterSubmit={afterSubmit}
-      /> */}
-
-      <TaskForm editMode={editMode} afterSubmit={afterSubmit} />
+      />
     </Modal>
   );
 };
