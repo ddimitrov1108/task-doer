ae392ed95c693f138753f50aa0bb5e673ad53e8c
Daniel Dimitrov <ddimitrov1108@gmail.com>
2024-01-11 11:50:40 +0200

    delete: unused providerd

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,8 +1,7 @@
 export { default as AuthProvider } from "./AuthProvider";
-export { default as StorageProvider } from "./StorageProvider";
-export { default as ModalsProvider } from "./ModalsProvider";
+export { default as ProjectProvider } from "./ProjectProvider";
+export { default as LabelProvider } from "./LabelProvider";
 export { default as TaskProvider } from "./TaskProvider";
-
-export { StorageContext } from "./StorageProvider";
-export { ModalsContext } from "./ModalsProvider";
+export { ProjectContext } from "./ProjectProvider";
+export { LabelContext } from "./LabelProvider";
 export { TaskContext } from "./TaskProvider";
