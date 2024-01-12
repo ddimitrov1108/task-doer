f20ca921e56714418a49f14a90ea8c231ebd92ca
Daniel Dimitrov <ddimitrov1108@gmail.com>
2024-01-12 11:30:48 +0200

    add: mysql event for task resets

---
+++ prisma/reset-repeat-tasks.sql
@@ -0,0 +1,22 @@
+/*
+    This Event resets all tasks in the Task table if their
+    repeat and complete fields are set to true. All of those tasks
+    will update:
+        - completed = false;
+        - due_date = currdate()
+*/
+
+DELIMITER //
+
+CREATE EVENT reset_completed_tasks
+ON SCHEDULE EVERY 1 DAY
+STARTS CURRENT_TIMESTAMP
+COMMENT 'Reset completed tasks every day at midnight'
+DO
+BEGIN
+    UPDATE your_tasks_table
+    SET completed = 0, due_date = DATE_ADD(CURDATE(), INTERVAL 1 DAY)
+    WHERE completed = 1 AND repeat_task = 1;
+END//
+
+DELIMITER ;
