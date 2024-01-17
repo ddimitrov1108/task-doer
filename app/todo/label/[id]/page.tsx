729d9c397d1bc2a5784c3450b4aea82d9ba19463
Daniel D <79416039+ddimitrov1108@users.noreply.github.com>
2024-01-18 00:19:26 +0200

    modified:   app/todo/label/[id]/layout.tsx modified:   app/todo/label/[id]/page.tsx

--- app/todo/label/[id]/page.tsx
+++ app/todo/label/[id]/page.tsx
@@ -1,6 +1,19 @@
+import { getUserFromServerSession } from "@/lib/auth";
 import { NextRouteParams } from "@/lib/interfaces";
+import { isUUID } from "@/lib/utils";
 
-const LabelPage = ({ params }: NextRouteParams) => {
+const LabelPage = async ({ params }: NextRouteParams) => {
+  if (!params?.id || !isUUID(params?.id))
+    return (await import("next/navigation")).notFound();
+
+  const user = await getUserFromServerSession();
+
+  if (!user) return (await import("next/navigation")).redirect("/");
+
+  const label = await (
+    await import("@/db/LabelController")
+  ).default.get(user.id, params.id);
+
   return <div>{params?.id}</div>;
 };
 export default LabelPage;
