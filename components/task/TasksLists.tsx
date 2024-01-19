31f2ac50657a68c793a275ab5b76a3342e9bf1d7
Daniel Dimitrov <ddimitrov1108@gmail.com>
2024-01-19 18:27:54 +0200

    code cleanup

--- components/task/TasksLists.tsx
+++ components/task/TasksLists.tsx
@@ -1,6 +1,5 @@
 import { ITask } from "@/lib/interfaces";
 import { isFuture, isPast, isToday } from "date-fns";
-import { sortByDate } from "@/lib/utils";
 import ListOfTasks from "./ListOfTasks";
 import dynamic from "next/dynamic";
 
@@ -14,31 +13,25 @@ interface Props {
 }
 
 const TasksLists = ({ tasks }: Props) => {
-  const pastDueTasks = sortByDate(
-    tasks.filter(
-      (e) => !e.completed && isPast(e.due_date) && !isToday(e.due_date)
-    )
+  const pastDueTasks = tasks.filter(
+    (e) => !e.completed && isPast(e.due_date) && !isToday(e.due_date)
   );
 
-  const importantTasks = sortByDate(
-    tasks.filter(
-      (e) =>
-        e.important &&
-        !e.completed &&
-        (isToday(e.due_date) || isFuture(e.due_date))
-    )
+  const importantTasks = tasks.filter(
+    (e) =>
+      e.important &&
+      !e.completed &&
+      (isToday(e.due_date) || isFuture(e.due_date))
   );
 
-  const activeTasks = sortByDate(
-    tasks.filter(
-      (e) =>
-        !e.important &&
-        !e.completed &&
-        (isToday(e.due_date) || isFuture(e.due_date))
-    )
+  const activeTasks = tasks.filter(
+    (e) =>
+      !e.important &&
+      !e.completed &&
+      (isToday(e.due_date) || isFuture(e.due_date))
   );
 
-  const completedTasks = sortByDate(tasks.filter((e) => e.completed));
+  const completedTasks = tasks.filter((e) => e.completed);
 
   return (
     <div className="grid gap-6">
@@ -46,7 +39,8 @@ const TasksLists = ({ tasks }: Props) => {
         <EmptyTasksStatus />
       ) : !pastDueTasks.length &&
         !importantTasks.length &&
-        !activeTasks.length && completedTasks.length ? (
+        !activeTasks.length &&
+        completedTasks.length ? (
         <>
           <CompletedTasksStatus />
           <ListOfTasks listTitle="Completed Tasks" tasks={completedTasks} />
