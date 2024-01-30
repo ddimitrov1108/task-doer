af50e852e8d5ddecfb14d8815d9fd00bccc4277e
Daniel Dimitrov <ddimitrov1108@gmail.com>
2024-01-30 18:48:33 +0200

    sort optimization

--- components/task/TasksLists.tsx
+++ components/task/TasksLists.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { ITask } from "@/lib/interfaces";
-import { isFuture, isPast, isToday } from "date-fns";
+import { compareAsc, compareDesc, isFuture, isPast, isToday } from "date-fns";
 import dynamic from "next/dynamic";
 import { useSearchParams } from "next/navigation";
-import { sortBy } from "@/lib/utils";
 
 const CompletedTasksStatus = dynamic(
   () => import("./components/status/CompletedTasksStatus")
@@ -22,6 +21,22 @@ interface Props {
   showCompleteStatus?: boolean;
 }
 
+const sortBy = (arr: ITask[], by: string | undefined) => {
+  if (!arr || !arr.length) return [];
+
+  switch (by) {
+    case "asc":
+      return arr.sort((a, b) => a.name.localeCompare(b.name));
+    case "desc":
+      return arr.sort((a, b) => b.name.localeCompare(a.name));
+    case "oldest":
+      return arr.sort((a, b) => compareAsc(a.dueDate, b.dueDate));
+    case "newest":
+    default:
+      return arr.sort((a, b) => compareDesc(a.dueDate, b.dueDate));
+  }
+};
+
 const TasksLists = ({
   tasks,
   openCompletedTab = false,
@@ -34,9 +49,12 @@ const TasksLists = ({
   const searchParam = searchParams.get("search")?.toLowerCase();
   const sortParam = searchParams.get("sort")?.toLowerCase();
 
-  const filteredTasks = searchParam
-    ? tasks.filter((task) => task.name.toLowerCase().includes(searchParam))
-    : tasks;
+  const filteredTasks = sortBy(
+    searchParam
+      ? tasks.filter((task) => task.name.toLowerCase().includes(searchParam))
+      : tasks,
+    sortParam
+  );
 
   const pastDueTasks = filteredTasks.filter(
     (e) => !e.completed && isPast(e.dueDate) && !isToday(e.dueDate)
@@ -77,22 +95,13 @@ const TasksLists = ({
               <CompletedTasksStatus />
             ) : null}
 
-            <ListOfTasks
-              listTitle="Past Due Tasks"
-              tasks={sortBy(pastDueTasks, sortParam)}
-            />
-            <ListOfTasks
-              listTitle="Important Tasks"
-              tasks={sortBy(importantTasks, sortParam)}
-            />
-            <ListOfTasks
-              listTitle="Active Tasks"
-              tasks={sortBy(activeTasks, sortParam)}
-            />
+            <ListOfTasks listTitle="Past Due Tasks" tasks={pastDueTasks} />
+            <ListOfTasks listTitle="Important Tasks" tasks={importantTasks} />
+            <ListOfTasks listTitle="Active Tasks" tasks={activeTasks} />
             <ListOfTasks
               open={openCompletedTab ? true : !!searchParam}
               listTitle="Completed Tasks"
-              tasks={sortBy(completedTasks, sortParam)}
+              tasks={completedTasks}
             />
           </>
         )}
